3b9cec35ce539325736605d09df260d5cbf381ea
ragq <ragq1991@gmail.com>
2021-06-28 13:48:49 +0600

    Decomposition func "Main".

--- main.py
+++ main.py
@@ -48,42 +48,63 @@ def no_access(my_vk, user_id):
                              "response_type=token&v=5.131")
 
 
+def user_say_yes(test_db, my_vk, user_id):
+    # получим параметры пользователя для будущего поиска из БД
+    user_data = test_db.get_params(user_id)
+    # если в БД нет попробуем взять с аккаунта Вконтакте
+    if user_data is False:
+        user_data = my_vk.get_user_data(user_id)
+        # получили - запишем в БД
+        if bool(user_data):
+            send(test_db, my_vk, user_id, user_data)
+        # не получили - сообщим что доступа нет и нужен токен
+        else:
+            no_access(my_vk, user_id)
+    # нашелся такой в БД, отправим ему анкеты
+    else:
+        send(test_db, my_vk, user_id, user_data)
+
+
+def user_send_token(test_db, my_vk, user_id, request):
+    # сообщим пользователю что получили его токен
+    my_vk.write_msg(user_id, "Ссылку получил, обрабатываю...")
+    token = request.partition('access_token=')[2].partition('&')[0]
+    # получим данные из аккаунта Вконтакте с токеном пользователя
+    user_data = my_vk.get_user_data(user_id, token)
+    if bool(user_data):
+        # полуили данные - отправим анкеты
+        send(test_db, my_vk, user_id, user_data)
+    else:
+        # не получили - сообщим пользователю
+        no_access(my_vk, user_id)
+
+
 def main():
+    # создадм объект для общания с БД
     test_db = DBForBot(stt.db_name, stt.db_user, stt.db_pass, stt.db_host)
+    # создадим объект для API Вконтакте
     my_vk = VkMessenger(stt.vk_token, stt.vk_login, stt.vk_pass, stt.vk_idapp)
+    # слушаем входящие сообщения
     for event in my_vk.longpoll.listen():
+        # если что-то написали
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            # если это "что-то" написали Нам
             if event.to_me:
+                # получим это "что-то" и id отправителя
                 request = event.text
                 user_id = event.user_id
+                # если с нами поздоровались - поздороваемся в ответ
                 if request.lower() in list_hello:
                     my_vk.write_msg(user_id, "Хай.")
                     my_vk.write_msg(user_id, "Найдем тебе собеседника?")
                 elif request.lower() in list_yes:
-                    user_data = test_db.get_params(user_id)
-                    if user_data is False:
-                        user_data = my_vk.get_user_data(user_id)
-                        if bool(user_data):
-                            send(test_db, my_vk, user_id, user_data)
-                        else:
-                            no_access(my_vk, user_id)
-                    else:
-                        send(test_db, my_vk, user_id, user_data)
-                elif 'access_token' in request:
-                    my_vk.write_msg(user_id, "Ссылку получил, обрабатываю...")
-                    token = request.partition('access_token=')[2].partition('&')[0]
-                    user_data = my_vk.get_user_data(user_id, token)
-                    if bool(user_data):
-                        send(test_db, my_vk, user_id, user_data)
-                    else:
-                        no_access(my_vk, user_id)
+                    user_say_yes(test_db, my_vk, user_id)
                 elif request.lower() in list_no:
                     my_vk.write_msg(event.user_id, "Ну тогда пока.")
+                elif 'access_token' in request:
+                    user_send_token(test_db, my_vk, user_id, request)
                 else:
-                    try:
-                        my_vk.write_msg(event.user_id, "Не поняла вашего ответа...")
-                    except ValueError:
-                        print('321')
+                    my_vk.write_msg(event.user_id, "Не поняла вашего ответа...")
 
 
 if __name__ == "__main__":
